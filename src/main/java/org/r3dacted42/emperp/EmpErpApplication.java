722382fdfa3b70c106df64579f173840906841fa
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-11-20 02:22:33 +0530

    backend init commit

---
+++ src/main/java/org/r3dacted42/emperp/EmpErpApplication.java
@@ -0,0 +1,13 @@
+package org.r3dacted42.emperp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/r3dacted42/emperp/EmpErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.r3dacted42.emperp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
