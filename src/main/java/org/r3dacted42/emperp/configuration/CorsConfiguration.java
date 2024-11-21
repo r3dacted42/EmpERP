2fe0f43fff4c7053c847086a7f8d14c3b40167f0
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-11-22 03:10:06 +0530

    added cors

---
+++ src/main/java/org/r3dacted42/emperp/configuration/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package org.r3dacted42.emperp.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
