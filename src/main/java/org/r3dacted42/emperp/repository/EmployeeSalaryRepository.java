072f038ff87e6e9e65bebf01a81cc41f1bef2bb3
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-11-22 01:18:08 +0530

    frontend init commit
    hello react, i'm not a big fan :)

--- src/main/java/org/r3dacted42/emperp/repository/EmployeeSalaryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.r3dacted42.emperp.repository;
-
-import org.r3dacted42.emperp.entity.EmployeeSalary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeSalaryRepository extends JpaRepository<EmployeeSalary, Long> {
-}
